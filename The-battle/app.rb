2b8b981222b49278abb5c3a526938725d046cec2
Richard Turney <r.turney93@gmail.com>
2021-01-21 14:59:43 +0000

    fixed broken submit button

--- The-battle/app.rb
+++ The-battle/app.rb
@@ -1,22 +1,24 @@
 require 'sinatra'
 
+enable :sessions
+
 class Battle < Sinatra::Base
 
   get '/' do
     erb(:index)
-
   end
 
-  get '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-
-    erb :play
-
+  post '/names' do
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
   end
 
-  
-
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
+  end
 
   run! if app_file == $0
 end
